31dc557b8adfe5646b8003ef229536fd23fde0c7
Yuri <yurisokolenko@gmail.com>
2020-04-15 19:29:43 +0300

    added microservice for creating each word separately

---
+++ lab-4-adjective/src/main/java/com/tlab/lab4adjective/controller/AdjectiveController.java
@@ -0,0 +1,22 @@
+package com.tlab.lab4adjective.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class AdjectiveController {
+
+    private final Random random = new Random();
+
+    @Value("${words}") String[] words;
+
+    @GetMapping("/")
+    public String getWord() {
+        return words[random.nextInt(words.length)];
+    }
+
+}
---
+++ lab-4-adjective/src/test/java/com/tlab/lab4adjective/Lab4AdjectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlab.lab4adjective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4AdjectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
